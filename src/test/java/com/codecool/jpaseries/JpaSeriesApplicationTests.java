cf386e85cd59895e64080cca507aa5156527b388
Tharegon <dombovari11david@gmaa>
2020-10-22 10:52:52 +0200

    Initial commit

---
+++ src/test/java/com/codecool/jpaseries/JpaSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.jpaseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaSeriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
